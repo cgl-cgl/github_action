d369f0cbc4a80bab426efbe30c4395c3c0d560d2
cgl-cgl <pudding.cgl@outlook.com>
2022-01-09 18:30:34 +0800

    v2

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-def test_run(input1):
-    if input1 == 'll':
-        print('Hello World')
-    else:
-        print('!')
+def test_run():
+    # if input1 == 'll':
+    #     print('Hello World')
+    # else:
+    #     print('!')
+    print('Hello World')
 
 if __name__ == '__main__':
-    test_run(input1)
+    test_run()
