4cf5077b9df72fc4c47871593ab0afcf17677b1d
cgl-cgl <pudding.cgl@outlook.com>
2022-01-09 19:17:55 +0800

    inputs

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import sys
 import argparse
+
 path = sys.path[0]
 parser=argparse.ArgumentParser(description='model')
 parser.add_argument('input1',type=str,help='input id')
 args=parser.parse_args()
 input1=args.input1
+
 def test_run(input1):
     if input1 == 'll':
         print('Hello World')
