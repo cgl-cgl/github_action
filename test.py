60cb1a4790ea471890b69cb8b89782867e7d9bbe
cgl-cgl <pudding.cgl@outlook.com>
2022-01-09 19:00:39 +0800

    param add

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-def test_run():
-    # if input1 == 'll':
-    #     print('Hello World')
-    # else:
-    #     print('!')
-    print('Hello World')
+def test_run(input1):
+    if input1 == 'll':
+        print('Hello World')
+    else:
+        print('!')
+    # print('Hello World')
 
 if __name__ == '__main__':
-    test_run()
+    test_run(input1)
